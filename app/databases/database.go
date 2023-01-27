1208758ee07d4761a05fb1d6f8c976e43e9fb007
Rizqi Wijaya <rizwijaya@github.com>
2023-01-27 21:33:24 +0700

    Create driver for 3 database type

---
+++ app/databases/database.go
@@ -0,0 +1,73 @@
+package database
+
+import (
+	"FastStarter/app/config"
+	"context"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var database map[string]interface{}
+
+func MySql(config config.LoadConfig) *gorm.DB {
+	dsn := config.Database.Username + ":" + config.Database.Password + "@tcp(" + config.Database.Host + ":" + config.Database.Port + ")/" + config.Database.Name + "?charset=utf8&parseTime=True&loc=Local"
+	Db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return Db
+}
+
+func Postgres(config config.LoadConfig) *gorm.DB {
+	dsn := "host=" + config.Database.Host + " user=" + config.Database.Username + " password=" + config.Database.Password + " dbname=" + config.Database.Name + " port=" + config.Database.Port + " sslmode=disable TimeZone=Asia/Jakarta"
+	Db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return Db
+}
+
+func MongoDb(config config.LoadConfig) *mongo.Database {
+	var clientOptions *options.ClientOptions
+	ctx := context.TODO()
+	if config.Database.Username == "" && config.Database.Password == "" {
+		clientOptions = options.Client().ApplyURI("mongodb://" + config.Database.Host + ":" + config.Database.Port)
+	} else {
+		clientOptions = options.Client().ApplyURI("mongodb://" + config.Database.Username + ":" + config.Database.Password + "@" + config.Database.Host + ":" + config.Database.Port)
+	}
+	client, err := mongo.Connect(ctx, clientOptions)
+	if err != nil {
+		log.Println(err)
+		return nil
+	}
+	db := client.Database(config.Database.Name)
+	return db
+}
+
+func NewDatabase() (map[string]interface{}, error) {
+	config, err := config.New()
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+	if config.Database.Driver == "mysql" {
+		database["type"] = "mysql"
+		database["connection"] = MySql(config)
+		return database, nil
+	} else if config.Database.Driver == "postgres" {
+		database["type"] = "postgres"
+		database["connection"] = Postgres(config)
+	} else if config.Database.Driver == "mongoDb" {
+		database["type"] = "mongoDb"
+		database["connection"] = MongoDb(config)
+	}
+	return database, nil
+}
--- go.mod
+++ go.mod
@@ -2,9 +2,33 @@ module FastStarter
 
 go 1.18
 
+require gorm.io/driver/mysql v1.4.5
+
+require (
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.2.0 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.11.1 // indirect
+	golang.org/x/crypto v0.4.0 // indirect
+	golang.org/x/sync v0.0.0-20220923202941-7f9b1623fab7 // indirect
+	golang.org/x/text v0.5.0 // indirect
+)
+
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/gofiber/fiber/v2 v2.41.0 // indirect
+	github.com/golobby/cast v1.3.3 // indirect
+	github.com/golobby/dotenv v1.3.2 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.15.15 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
@@ -14,4 +38,6 @@ require (
 	github.com/valyala/fasthttp v1.44.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
+	gorm.io/driver/postgres v1.4.6
+	gorm.io/gorm v1.24.3 // indirect
 )
