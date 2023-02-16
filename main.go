953783245779a8ff7d38b7c5a7d16a3687980348
Rizqi Wijaya <rizwijaya@github.com>
2023-02-16 21:42:39 +0700

    Create Fungsi Main in Codebase

--- main.go
+++ main.go
@@ -1,11 +1,38 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"FastStarter/app/config"
+	database "FastStarter/app/databases"
+	routesV1 "FastStarter/modules/v1/routes"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/template/html"
+)
+
+func new() (*fiber.App, map[string]interface{}) {
+	database, err := database.NewDatabase()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	engine := html.New("./public/templates", ".html")
+	router := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	router.Use(cors.New())
+
+	return router, database
+}
 
 func main() {
-	router := fiber.New()
-	router.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
-	})
-	router.Listen(":3000")
+	config, err := config.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := routesV1.NewRouter(new())
+	router.Listen(config.App.Url + ":" + config.App.Port)
 }
