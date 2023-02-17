01566e12219d5e3c0220c44321a065601a298318
Rizqi Wijaya <rizwijaya@github.com>
2023-02-17 13:55:45 +0700

    Create JWT Token Package Generate

---
+++ pkg/jwt/token.go
@@ -0,0 +1,41 @@
+package token
+
+import (
+	"FastStarter/app/config"
+	"errors"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+func ValidateToken(encodedToken string) (*jwt.Token, error) {
+	conf, _ := config.New()
+	token, err := jwt.Parse(encodedToken, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("Invalid token")
+		}
+
+		return []byte(conf.App.Secret_key), nil
+	})
+
+	if err != nil {
+		return token, err
+	}
+
+	return token, nil
+}
+
+func GenerateToken(userID int) (string, error) {
+	conf, _ := config.New()
+	claim := jwt.MapClaims{}
+	claim["user_id"] = userID
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
+
+	signedToken, err := token.SignedString(conf.App.Secret_key)
+	if err != nil {
+		return signedToken, err
+	}
+
+	return signedToken, nil
+}
