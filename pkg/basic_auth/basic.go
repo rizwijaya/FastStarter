3b3c928a36ece7f1eb02cc1a6b2e08312757595d
Rizqi Wijaya <rizwijaya@github.com>
2023-02-17 13:44:43 +0700

    Create Basic Auth Package

---
+++ pkg/basic_auth/basic.go
@@ -0,0 +1,29 @@
+package basic
+
+import (
+	"FastStarter/app/config"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/basicauth"
+)
+
+func Auth(conf config.LoadConfig) func(*fiber.Ctx) error {
+	return basicauth.New(basicauth.Config{
+		Users: map[string]string{
+			conf.BasicAuth.Username: conf.BasicAuth.Password,
+		},
+		Realm: "Restricted",
+		Authorizer: func(user, pass string) bool {
+			if user == conf.BasicAuth.Username && pass == conf.BasicAuth.Password {
+				return true
+			}
+			return false
+		},
+		Unauthorized: func(c *fiber.Ctx) error {
+			return c.SendFile("./public/templates/error/forbidden.html")
+		},
+		ContextUsername: "_user",
+		ContextPassword: "_pass",
+	},
+	)
+}
